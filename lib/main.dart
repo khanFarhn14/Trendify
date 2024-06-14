115f3bffbf6c042e304079408b10b2620c9b3f2b
Khan Farhan <thecubeshade21@gmail.com>
2024-06-14 16:52:29 +0530

    added local storage and sessions

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:trendify/pages/login_page.dart';
+import 'package:trendify/cache/cache_storage.dart';
 import 'package:trendify/providers/provider_products_list.dart';
+import 'package:trendify/resources.dart';
+import 'package:trendify/routes/routes.dart';
+import 'package:trendify/routes/routes_name.dart';
+import 'package:trendify/widgets/style_components.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await checkUserLoggedInStatus();
   runApp(const MyApp());
+}
+
+bool _isLoggedIn = false;
+
+Future<void> checkUserLoggedInStatus() async {
+  await LocalStorage.getUserLoggedInStatus().then((value){
+    if(value != null){
+      clearPrint("Is user logged in $value");
+        _isLoggedIn = value;
+    }
+  });
+
+  clearPrint("Checking logged in status after checking $_isLoggedIn");
 }
 
 class MyApp extends StatelessWidget {
@@ -16,9 +35,30 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (context) => ProductList())
       ],
-      child: const MaterialApp(
+      child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: LoginPage(),
+        theme: ThemeData(
+          colorSchemeSeed: ColorComponents.blackColor,
+          scaffoldBackgroundColor: ColorComponents.primaryColor,
+          textTheme: Theme.of(context).textTheme.apply(
+            bodyColor: ColorComponents.blackColor,
+          ),
+
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(            
+              backgroundColor: ColorComponents.blackColor,
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50),
+              ),
+              shadowColor: ColorComponents.black40LightColor,
+            )
+          ),
+          bottomAppBarTheme: const BottomAppBarTheme(color: ColorComponents.primaryColor)        
+        ),
+        // home: const LoginPage(),
+        initialRoute: _isLoggedIn ? RouteName.home : RouteName.login,
+        onGenerateRoute: Routes.generateRoute,
       ),
     );
   }
