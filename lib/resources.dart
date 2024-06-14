0ed07e82b3f695a8d5d3a62378371d1e5348f6a2
Khan Farhan <thecubeshade21@gmail.com>
2024-06-14 16:52:39 +0530

    assets resources

---
+++ lib/resources.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+clearPrint(String message) => debugPrint("-------------------------------- $message --------------------------------");
+
+class ImageResources{
+  static const String loginPageIllustration = 'assets/images/login_page_illustration.png';
+}
+
+class IconsResources{
+  static const String iconSearch = 'assets/images/icons/icon_search.png';
+  static const String iconArrow = 'assets/images/icons/icon_arrow.png';
+  static const String iconCancel = 'assets/images/icons/icon_cancel.png';
+  static const String iconCart = 'assets/images/icons/icon_cart.png';
+  static const String iconFilter = 'assets/images/icons/icon_filter.png';
+  static const String iconOption = 'assets/images/icons/icon_option.png';
+  static const String iconSort = 'assets/images/icons/icon_sort.png';
+}
